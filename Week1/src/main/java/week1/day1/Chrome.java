067747553667564982cf48e1e9842801ba5f7606
suriyaaws2020 <60845269+suriyaaws2020@users.noreply.github.com>
2026-01-04 08:39:37 +0530

    Add files via upload
    Week1 assignment

---
+++ Week1/src/main/java/week1/day1/Chrome.java
@@ -0,0 +1,32 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		
+		
+		float version = 91.0f;
+		
+		String developer = "Google";
+		
+		boolean isBeta=false;
+		
+		int releaseYear=2008;
+		
+		char shortcutKey = 'C';
+		
+		System.out.println("Browser Google Chrome");
+		
+		System.out.println("Chrome Version: "+version);
+		
+		System.out.println("Developed by "+developer);
+		
+		System.out.println("Is it a Beta version - "+isBeta);
+		
+		System.out.println("Release year: " + releaseYear);
+		
+		System.out.println("Keyboard shortcut - "+shortcutKey);
+
+	}
+
+}
